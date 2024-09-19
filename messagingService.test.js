1980204ef15c2efdef84e2c62fbd3fefd758d1e2
Eli Boninger <eboninger@brainstation.io>
2024-09-19 11:14:43 -0400

    intentionally fail test case

--- messagingService.test.js
+++ messagingService.test.js
@@ -3,7 +3,7 @@ import { sum } from "./sum";
 
 describe('sum function', () => {
     it('called with 0 and 1 is equal to 1', () => {
-        expect(sum(0, 1)).toBe(1);
+        expect(sum(0, 1)).toBe(0);
     });
 
     it('called with 0 and -1 is equal to -1', () => {
